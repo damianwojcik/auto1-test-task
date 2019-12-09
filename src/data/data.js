7f81506860f039ad79ae267b5a453f735a236afd
khamian <khamian@gmail.com>
2019-12-09 20:04:13 +0100

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,19 @@
+export const list = [
+  {
+    id: '1',
+    firstname: 'Damian',
+    lastname: 'Wójcik',
+    avatarUrl: 'string',
+    email: 'khamian@gmail.com',
+    phone: '792225950',
+    hasPremium: true,
+    bids: [
+      {
+        id: '123',
+        carTitle: 'Fiat',
+        amount: '521',
+        created: '09-12-2019'
+      }
+    ]
+  }
+];
